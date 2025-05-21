15779abf1bd1049efc3cf9705b0bcb9460fb2527
Julia P. Zang <juliapz2008@gmail.com>
2025-05-21 16:50:48 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -74,6 +74,29 @@ app.put('/movies/:id', async (req, res) => {
    res.status(200).send();
 });
 
+app.delete('/movies/:id', async (req, res) => {
+   const id = Number(req.params.id);
+
+
+   try {
+
+      const movie = await prisma.movie.findUnique({
+         where: { id }
+      })
+
+      if (!movie) {
+         res.status(404).send({ message: 'Filme não encontrado' });
+      }
+
+      await prisma.movie.delete({ where: { id } });
+
+   } catch (error) {
+      res.status(500).send({ message: 'Falha ao remover o registro' });
+   }
+
+   res.status(200).send();
+});
+
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
