c7a5c9e522b1cdc0d16d6c822128a5e9c344d7b9
Julia P. Zang <juliapz2008@gmail.com>
2025-05-21 16:37:33 -0300

    adicionada atualizalção de filmes

--- src/server.ts
+++ src/server.ts
@@ -21,35 +21,57 @@ app.get('/movies', async (_, res) => {
 });
 
 app.post("/movies", async (req, res) => {
- const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
- try {
-    const movieWithSameTitle = await prisma.movie.findFirst({
-       where: { 
-           title: { equals: title, mode: "insensitive" }
-       }, 
-   });
+   try {
+      const movieWithSameTitle = await prisma.movie.findFirst({
+         where: {
+            title: { equals: title, mode: "insensitive" }
+         },
+      });
 
-   if (movieWithSameTitle) {
-      res
-        .status(409)
-        .send({ message: "Já existe um filme com esse título" });
+      if (movieWithSameTitle) {
+         res
+            .status(409)
+            .send({ message: "Já existe um filme com esse título" });
+      }
+
+      await prisma.movie.create({
+         data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date),
+         },
+      });
+   } catch (error) {
+      res.status(500).send({ message: "Falha ao cadastrar um filme" });
    }
 
-   await prisma.movie.create({
-     data: {
-       title,
-       genre_id,
-       language_id,
-       oscar_count,
-       release_date: new Date(release_date),
-     },
-   });
- } catch (error) {
-   res.status(500).send({ message: "Falha ao cadastrar um filme" });
- }
+   res.status(201).send();
+});
 
- res.status(201).send();
+app.put('/movies/:id', async (req, res) => {
+   const id = Number(req.params.id);
+
+   try {
+      const movie = await prisma.movie.findUnique({
+         where: { id },
+      });
+
+      if (!movie) {
+         res.status(404).send({ message: "Filme não encontrado" });
+      }
+
+      const data = { ...req.body };
+      data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+      await prisma.movie.update({ where: { id }, data });
+   } catch (error) {
+      res.status(500).send({ message: "Falha ao atualizar o registro" });
+   }
+   res.status(200).send();
 });
 
 app.listen(port, () => {
